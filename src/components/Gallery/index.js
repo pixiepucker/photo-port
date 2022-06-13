ceb074db85173c32dce25140a4f928f991b0dfaa
pixiepucker <autumnoffic@gmail.com>
2022-06-13 17:07:32 -0500

    added Gallery component w hooks

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from '../../assets/small/commercial/0.jpg';
+
+function Gallery(props) {
+  const currentCategory = {
+    name: 'commercial',
+    description:
+      'Photos of grocery stores, food trucks, and other commercial projects',
+  };
+
+  return (
+    <section>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.name}</p>
+      <div>
+        <img
+          src={photo}
+          className="img-thumbnail mx-1"
+          alt="Commerical Example"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Gallery;
