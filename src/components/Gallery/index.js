e88e6c426f63155a6c9a371d58857883b640906d
pixiepucker <autumnoffic@gmail.com>
2022-06-13 17:31:50 -0500

    snapshot 20.3

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,27 +1,15 @@
 import React from 'react';
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg';
 
 function Gallery(props) {
-  const currentCategory = {
-    name: 'commercial',
-    description:
-      'Photos of grocery stores, food trucks, and other commercial projects',
-  };
-
+  const { currentCategory } = props;
   return (
     <section>
-      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.name}</p>
-      <div>
-        <img
-          src={photo}
-          className="img-thumbnail mx-1"
-          alt="Commerical Example"
-        />
-      </div>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
     </section>
   );
 }
-
 export default Gallery;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,22 +1,24 @@
-import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-  const { categories = [], setCurrentCategory, currentCategory } = props;
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
+
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
 
   return (
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera">
-            {' '}
-            📸
-          </span>{' '}
-          Oh Snap!
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
@@ -26,14 +28,16 @@ function Nav() {
               About me
             </a>
           </li>
-          <li className="mx-2">
-            <span>Contact</span>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick('Contact')}>
+              Contact
+            </span>
           </li>
-          {categories.map(category => (
+          {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && 'nanActive'
-              }`}
+                currentCategory.name === category.name
+                }`}
               key={category.name}
             >
               <span
